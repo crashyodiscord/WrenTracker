974fff182d1e2756e712b75659ee6d0b8f67578f
Jude <juderay12@gmail.com>
2025-06-19 23:53:14 +0100

    First Version of WrenTracker, person.html needs finishing

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect, url_for
+import json, os
+
+app = Flask(__name__)
+DATA_FILE = "data/locations.json"
+PASSWORDS = {"Ben": "Tottenham1", "John": "Fortunte2", "Jude": "Ray3", "Laoch": "Kennedy4", "Peter": "Richardson5"}
+
+def load_locations():
+    if not os.path.exists(DATA_FILE):
+        with open(DATA_FILE, 'w') as f:
+            json.dump({}, f)
+    with open(DATA_FILE) as f:
+        return json.load(f)
+
+def save_locations(data):
+    with open(DATA_FILE, 'w') as f:
+        json.dump(data, f)
+
+@app.route("/")
+def index():
+    people = [
+        {"id": "Ben", "image": "Ben.png"},
+        {"id": "John", "image": "John.png"},
+        {"id": "Jude", "image": "Jude.png"},
+        {"id": "Laoch", "image": "Laoch.png"},
+        {"id": "Peter", "image": "Peter.png"}
+    ]
+    return render_template("index.html", people=people)
+
+@app.route("/<person>")
+def person_page(person):
+    locations = load_locations()
+    loc = locations.get(person, {"lat": 0, "lng": 0})
+    return render_template("person.html", person=person, lat=loc["lat"], lng=loc["lng"])
+
+@app.route("/update/<person>", methods=["POST"])
+def update_location(person):
+    password = request.form["password"]
+    if password != PASSWORDS.get(person):
+        return "Incorrect password", 403
+
+    lat = request.form["lat"]
+    lng = request.form["lng"]
+
+    locations = load_locations()
+    locations[person] = {"lat": float(lat), "lng": float(lng)}
+    save_locations(locations)
+
+    return redirect(url_for('person_page', person=person))
+
+if __name__ == "__main__":
+    app.run(debug=True)
